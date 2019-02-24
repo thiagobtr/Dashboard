59b53e65b82023b2d60fb97b3a43f0ff341db754
thiagobtr <thiagobtr@yahoo.com.br>
2019-02-23 23:48:57 -0300

    23-02

--- Dashboard/appDrop.py
+++ Dashboard/appDrop.py
@@ -1,6 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table
 import pandas as pd
 import plotly.graph_objs as go
 
@@ -32,7 +33,7 @@ app.layout = html.Div(children=[
         html.Div([
             html.Div(
             style={'margin-top': '10'},
-            children=[
+            children=['Temporada:',
             dcc.Dropdown(
             id='dropdown-rodadas'
             ,options = [{'label':ano[0],'value': ano[1] }for ano in df_ano.itertuples(index=False)]
@@ -41,6 +42,7 @@ app.layout = html.Div(children=[
         ,html.Div(html.H1(''),className='three columns')
 
          ,html.Div(
+            style={'margin-top': '10'},
             children=['Selecione a Liga:',
                 dcc.RadioItems(
                     id='radio-liga',
@@ -58,22 +60,28 @@ app.layout = html.Div(children=[
         ,html.Div([
                 dcc.Graph(
                  id='Tabela-rank',
-                  )],className= 'eight columns')
 
+                 )],className= 'seven columns')
         ,html.Div([
                 dcc.Graph(
-                 id='Tabela-publico'
-                  )
+                 #html.Div(id='Tabela-publico'),
+                 id='Tabela-publico' )
+                  #dash_table.DataTable(
+                  #id = 'Tabela-publico',
+                  #columns = [{"name":i , 'id':i} for i in df_publico.columns],
+                  #data=df_publico.to_dict('rows'),
+                  #)
+
                 #update_table_publico(2015,'Premier_league')
 
-            ],className='three columns')
+            ],className='five columns')
 
         ,html.Div([
             dcc.Graph(
-                id='Tabela-art')],className='six columns')
+                id='Tabela-art')],className='seven columns')
         ,html.Div([
             dcc.Graph(
-                id='Tabela-assist')],className='six columns')
+                id='Dot-Plot-assist')],className='five columns')
 
 ],className='container')
 
@@ -85,12 +93,13 @@ app.layout = html.Div(children=[
 def update_graph_rank(temp,liga):
     df_temp = df_tabela[(df_tabela['Ano_inicio'] == temp) & (df_tabela['Liga']== liga)]
     temporada = df_temp['Temporada'].unique()
-    titulo = 'Pontuação por Time Temporada {}'.format(temporada)
+    titulo = 'Pontuação por Time Temporada {}'.format(temporada[0])
     return {
     'data':[go.Bar(
         x = df_temp['Time']
         ,y= df_temp['Pontos']
         ,text=''
+        ,marker={'color':'rgb(55, 83, 109)'}
         #,mode='lines+markers'
         )],
         'layout':go.Layout(
@@ -99,6 +108,7 @@ def update_graph_rank(temp,liga):
                     ,title = titulo
                     ,titlefont=dict(size=26,
                      color='#7f7f7f')
+                     ,xaxis=dict(tickangle=-45)
                      )
 
     }
@@ -114,7 +124,7 @@ def update_graph_art(temp,liga):
     df_art = df_art.sort_values(by='gols',ascending = True).tail(10)
 
     temporada = df_art['Temporada'].unique()
-    titulo = 'Lista de Artilheiros Temporada {}'.format(temporada)
+    titulo = 'Lista de Artilheiros Temporada {}'.format(temporada[0])
     return {
     'data':[go.Bar(
         x = df_art['gols']
@@ -132,6 +142,7 @@ def update_graph_art(temp,liga):
 
     }
 
+'''
 @app.callback(
     dash.dependencies.Output('Tabela-assist','figure'),
     [dash.dependencies.Input('dropdown-rodadas','value'),
@@ -143,7 +154,7 @@ def update_graph_ass(temp,liga):
     df_ass = df_ass.sort_values(by='ass',ascending = True).tail(10)
 
     temporada = df_ass['Temporada'].unique()
-    titulo = 'Lideres em Assistencias Temporada {}'.format(temporada)
+    titulo = 'Lideres em Assistencias Temporada {}'.format(temporada[0])
     return {
     'data':[go.Bar(
         x = df_ass['ass']
@@ -161,6 +172,7 @@ def update_graph_ass(temp,liga):
                      )
 
     }
+    '''
 @app.callback(
     dash.dependencies.Output('Tabela-publico','figure'),
     [dash.dependencies.Input('dropdown-rodadas','value'),
@@ -169,18 +181,75 @@ def update_graph_ass(temp,liga):
 def update_table_publico(temp,liga):
     df_pop = df_publico[(df_publico['Ano_inicio'] == temp) & (df_publico['Liga']== liga)]
     #lista os 10 jogadores que realizaram mais assistencias
-    df_pop = df_pop.sort_values(by='ass',ascending = True).tail(10)
-    max_rows =10
+    df_pop = df_pop.sort_values(by='Rank',ascending = True)#.head(20)
+    max_rows =20
     temporada = df_pop['Temporada'].unique()
-    titulo = 'Lideres em Assistencias Temporada {}'.format(temporada)
-    return html.Table(
-        [html.Tr([html.Th(col) for col in df_pop.columns])]+
-        [html.Tr([html.Td(df_pop.iloc[i][col])for col in df_pop.columns
-        ]) for i in range(min(len(df_pop),max_rows))]
+    titulo = 'Lideres de Público na Temporada {}'.format(temporada[0])
+    return {
+    'data':[go.Table(
+        columnwidth = [30,30,100],
+        header = dict(values=['Rank','Média','Time'],
+        fill = dict(color = 'rgb(55, 83, 109)'),
+        font = dict(color = 'white')),
+        cells = dict(values = [df_pop.Rank,round(df_pop.Media,3),df_pop.Time])
+        #,text= df_ass['ass'] +' Assistencias'
+        #,hoverinfo=text
+        #,mode='lines+markers'
+        )],
+        'layout':go.Layout(
+                    #hovermode='closest',
+                    title = titulo
+                    ,titlefont=dict(size=16,
+                     color='#7f7f7f')
+                     )
 
 
-    )
+    #dash_table.DataTable(
+    #id = 'Tabela-publico',
+    #columns = [{"name":i , 'id':i} for i in df_pop.columns],
+    #data=df.to_dict('rows'),
 
+    #)
+    }
+
+@app.callback(
+    dash.dependencies.Output('Dot-Plot-assist','figure'),
+    [dash.dependencies.Input('dropdown-rodadas','value'),
+    dash.dependencies.Input('radio-liga','value')]
+)
+def update_graph_ass(temp,liga):
+    df_saldo = df_tabela[(df_tabela['Ano_inicio'] == temp) & (df_tabela['Liga']== liga)]
+    #lista os 10 jogadores que realizaram mais assistencias
+    df_saldo = df_saldo.sort_values(by=['Pontos','Gols_pro'],ascending = True).tail(10)
+
+    temporada = df_saldo['Temporada'].unique()
+    titulo = 'Diferença de Gols a favor e contra. Temporada {}'.format(temporada[0])
+    trace1 = {'x':df_saldo['Gols_pro'],
+              'y':df_saldo['Time'],
+              'marker':{'color':'blue','size':12},
+              'mode':'markers',
+              'name': 'Gols a favor',
+              'type':'scatter'}
+
+    trace2 = {'x':df_saldo['Gols_con'],
+              'y':df_saldo['Time'],
+              'marker':{'color':'red','size':12},
+              'mode':'markers',
+              'name': 'Gols contra',
+              'type':'scatter'}
+    data =[trace1,trace2]
+
+    return {
+        'data':go.Figure(
+            data=data,
+            layout=go.Layout(
+                        #hovermode='closest',
+                        title = titulo
+                        ,titlefont=dict(size=16,
+                         color='#7f7f7f')
+                         ))
+
+    }
 
 if __name__ == '__main__':
     app.run_server(debug=True)
